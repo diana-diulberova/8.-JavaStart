d26fe5447cc9b20edc463dae9159e937c621bc1f
Diana <d.dyulberova@gmail.com>
2022-11-29 14:54:12 +0500

    task1

--- Seminar_1/Task1.java
+++ Seminar_1/Task1.java
@@ -1,5 +1,20 @@
+// Текст задачи:
+// Написать программу, которая запросит пользователя ввести <Имя> в 
+// консоли. Получит введенную строку и выведет в консоль сообщение “Привет, 
+// <Имя>!”
+
 package Seminar_1;
 
+import java.util.Scanner;
+
 public class Task1 {
-    
+    public static void main(String[] args) {
+        System.out.print("Input name: ");
+        Scanner iScanner = new Scanner(System.in);
+        String name = iScanner.nextLine();
+        System.out.printf(("Привет, %s\n"), name);
+        name = "Привет, " + name;
+        System.out.println(name);
+    }
+      
 }
